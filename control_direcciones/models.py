e7ac6738a25535fc8f0c76afd8d15a4d92b45d52
davidVzo <dvaldiviezonavarro@gmail.com>
2022-09-22 10:26:27 +0200

    Add table record

--- control_direcciones/models.py
+++ control_direcciones/models.py
@@ -1,9 +1,21 @@
 from odoo import models,fields
 
-class Control(models.Model):
+class Control(models.Model):    
     _name = "controles.control"
+
+    floor = fields.Integer("Piso")
     name = fields.Char("Nombre")
-    status = fields.Selection(selection=[("borrador","Borrador"),("hecho","Hecho")])
-    ip = fields.Char("Ip")
-    area = fields.Char("Area")
+    vlan = fields.Integer("Vlan")
+    current_ip = fields.Char("Ip_actual")
+    address = fields.Char("Direccion")
+    coordination = fields.Char("Coordinacion")
+    ci = fields.Char("Cedula_identidad")
+    host_name = fields.Char("Host_name")
+    user = fields.Char("Usuario")
+    ext_phone = fields.Integer("Extencion_telefonica")
+    comments = fields.Char("Observaciones")
+    
+
+    
+  
     
